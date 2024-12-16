b1c66f8386f434bdc5a58b6e68a300816112256e
samchi <seyoung0314@gmail.com>
2024-12-16 11:22:14 +0900

    initial commit

---
+++ src/test/java/com/spring/mvcproject/MvcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
